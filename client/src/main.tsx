06b5bfe24dfb405cb6a4e1001fabf7d0d71be510
Alexandre Ueda <contato@uedaodontologia.com.br>
2026-08-19 23:53:53 +0000

    Corrigido roteamento SPA com 404.html e suporte a subpasta para GitHub Pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// GitHub Pages SPA redirect handler
+(function () {
+  const params = new URLSearchParams(window.location.search);
+  const p = params.get("p");
+  if (p) {
+    window.history.replaceState(
+      null,
+      "",
+      window.location.pathname + p + (window.location.hash || "")
+    );
+  }
+})();
+
 createRoot(document.getElementById("root")!).render(<App />);
